4ad0aa338b022e8f22f39e23f7e3d1ae58f29d03
fiolentanna <tigranka@gmail.com>
2024-08-27 23:27:46 +0300

    2.1.3 added js file

---
+++ task_score.js
@@ -0,0 +1,10 @@
+let introductioScore = 30;
+let gitScore = 210;
+let jsScore = 300;
+let studentName = "Anna";
+let studentSurname  = "Medvedeva";
+let student  = studentName + ' ' + studentSurname;
+
+let averageScore = (introductioScore + gitScore + jsScore)/3;
+
+console.log(student + ' ' + averageScore);
