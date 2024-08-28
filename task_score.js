9245ed111b421d187eac992d94d8ebb8f62cf99e
fiolentanna <tigranka@gmail.com>
2024-08-28 14:35:22 +0300

    2.1.4 added changes to script

--- task_score.js
+++ task_score.js
@@ -3,8 +3,7 @@ let gitScore = 210;
 let jsScore = 300;
 let studentName = "Anna";
 let studentSurname  = "Medvedeva";
-let student  = studentName + ' ' + studentSurname;
 
 let averageScore = (introductioScore + gitScore + jsScore)/3;
 
-console.log(student + ' ' + averageScore);
+console.log(`${studentName.slice(0,1)}. ${studentSurname} ${averageScore}`);
